14d934dfe2107750b4b031515cdfc5602992cb8c
samuelct01 <93252364+samuelct01@users.noreply.github.com>
2024-02-23 13:30:40 +0000

    Update cloudcontroller.py

--- cloudcontroller.py
+++ cloudcontroller.py
@@ -3,8 +3,19 @@ import ssl
 import time
 import json
 import random
-
+from steps import step
+import socket
+ 
+# json object to hold current values
 instructions = None
+
+#turbine poition information
+angleToMove = 0
+currentRev = 0
+currentAngle = 0
+
+#saves the space in the sequence for stepper motor 
+inseq = 0
 # the onboard  code for controlling the turbine that communicates with the cloud 
 
 # when message is recieved write it to global variable instructions
@@ -23,17 +34,26 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.tls_set(
-            ca_certs="awsiottests/AmazonRootCA1.pem",
-            certfile="awsiottests/56dbe40dd150e8d4c49f6e9e5c7e092600ea0560a1317739cf49d370123db107-certificate.pem.crt",
-            keyfile="awsiottests/56dbe40dd150e8d4c49f6e9e5c7e092600ea0560a1317739cf49d370123db107-private.pem.key",
+            ca_certs="AmazonRootCA1.pem",
+            certfile="56dbe40dd150e8d4c49f6e9e5c7e092600ea0560a1317739cf49d370123db107-certificate.pem.crt",
+            keyfile="56dbe40dd150e8d4c49f6e9e5c7e092600ea0560a1317739cf49d370123db107-private.pem.key",
             cert_reqs=ssl.CERT_REQUIRED,
             tls_version=ssl.PROTOCOL_TLSv1_2
         )
 client.tls_insecure_set(True)
 client.connect("a2w8i53ab6a4ti-ats.iot.eu-west-2.amazonaws.com", 8883, 60)
 
+#connecting to the weather station
+
+socketsclient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+pi = ""
+port = 8000
+socketsclient.connect((ip, port))
 
 
+print('atm' + str(angleToMove))
+print('cr' + str(currentRev))
+print('ca' + str(currentAngle))
 
 #loop to handle incoming messages
 client.loop_start()
@@ -41,12 +61,13 @@ client.loop_start()
 
 
 while True:
+	
 
     data = {
     "direction": random.randint(0, 359),
     "speed": random.randint(5, 100),
-    "currentAngle": random.randint(0, 359),
-    "currentRev": random.randint(-359, 359)
+    "currentAngle": currentAngle,
+    "currentRev": currentRev
     }
     #publish data to topic
     client.publish('topic_2', json.dumps(data))
@@ -56,14 +77,24 @@ while True:
 
 
     #must allow time for subscription to be recieved
-    time.sleep(1)  
-
-    print("in rest of program")
-    print(instructions['currentAngle'])
-
-
-
-
-# Disconnect from the broker
-client.disconnect()
-#client.loop_stop()
+    time.sleep(3)  
+    
+    #if no instruction is recieved stop the program
+    if instructions == None:
+	    print("no message recieved")
+	    break
+    
+	#set position data vairables to attributes from message
+    angleToMove = instructions['angleToMove']
+    currentRev = instructions['currentRev']
+    currentAngle = instructions['currentAngle'] 
+    
+    print('atm' + str(angleToMove))
+    print('cr' + str(currentRev))
+    print('ca' + str(currentAngle))
+    
+    #rotate stepper motor and remember poition in step sequnece for next rotation
+    inseq = step(angleToMove, inseq)
+    
+    #reset instructions
+    instructions = None
